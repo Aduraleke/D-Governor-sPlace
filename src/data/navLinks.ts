f8e349a0b55a519a0d0a00b34f4fb0029ae989ef
Aduraleke <aduralekeodesanmi@gmail.com>
2025-09-08 15:31:39 +0100

    studio Page and other fixes

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -3,7 +3,7 @@ export const navLinks = [
   { name: "About", href: "/about", icon: "mdi:information-outline" },
   { name: "Apartments", href: "/apartments", icon: "mdi:bed-outline" },
   { name: "Bookings", href: "/bookings", icon: "mdi:calendar-check-outline" },
-//   { name: "Events", href: "#events", icon: "mdi:party-popper" },
+  { name: "VIP Studios", href: "/studio", icon: "mdi:camera-outline" },
   // { name: "Bar & Lounge", href: "#bar-lounge", icon: "mdi:glass-cocktail" },
 //   { name: "Gallery", href: "#gallery", icon: "mdi:image-multiple-outline" },
   { name: "Contact", href: "/contact", icon: "mdi:phone-outline" },
